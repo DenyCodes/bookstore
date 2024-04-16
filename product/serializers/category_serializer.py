26103f61d1cec9deff4b9277b754a9c1f5aae7be
Denis Oliveira <denisdev3loper@gmail.com>
2024-04-16 15:35:37 -0300

    viewsets

--- product/serializers/category_serializer.py
+++ product/serializers/category_serializer.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 
-from product.models.product import Product
 from product.models.Category import Category
 
-class ProductSerializer(serializers.ModelSerializer):
+
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = [
-            'title',
-            'description',
-            'price',
-            'active',
-            'category',
+            "title",
+            "slug",
+            "description",
+            "active",
         ]
+        extra_kwargs = {"slug": {"required": False}}
